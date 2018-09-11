b867cd0fb1950f9386e0010ee7fe55b87c05e4f4
Shawfe Sung <shawfe@techie.com>
2018-09-10 22:37:38 -0400

    Added main menu component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Routes } from '@angular/router';
 import { RoundRobinGridComponent } from './components/round-robin-grid/round-robin-grid.component'
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
 
 const routes: Routes = [
   {
     path: "rr", component: RoundRobinGridComponent,
-  }
+  },
+  {
+    path: "", component: MainMenuComponent,
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { RoundRobinGridComponent } from './components/round-robin-grid/round-robin-grid.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoundRobinGridComponent
+    RoundRobinGridComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
