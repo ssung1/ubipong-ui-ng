52b8829f859ed60542e1545a971d2a291945e043
Shawfe Sung <shawfe@techie.com>
2019-05-02 23:17:45 -0400

    WIP: Adding tournament creator part 7

--- src/app/components/tournament-list/tournament-list.component.spec.ts
+++ src/app/components/tournament-list/tournament-list.component.spec.ts
@@ -4,6 +4,7 @@ import { TournamentListComponent } from './tournament-list.component';
 
 describe('TournamentListComponent', () => {
   const tournamentName = 'Summer Games 2019';
+  const tournamentDate = '2019-06-20T12:00:00-0500';
 
   let component: TournamentListComponent;
   let fixture: ComponentFixture<TournamentListComponent>;
@@ -29,8 +30,8 @@ describe('TournamentListComponent', () => {
     component.tournamentList = [
       {
         name: tournamentName,
-        tournamentDate: '2019-06-20T12:00:00-0500'
-      }
+        tournamentDate: tournamentDate,
+      },
     ];
 
     fixture.detectChanges();
@@ -40,5 +41,40 @@ describe('TournamentListComponent', () => {
 
     expect(tournamentItem).toBeTruthy();
     expect(tournamentItem.textContent).toContain(tournamentName);
-  })
+  });
+
+  it('should hide new tournament form in the beginning', () => {
+    const dom = fixture.nativeElement;
+
+    const tournamentForm = dom.querySelector('.new-tournament-form');
+    expect(tournamentForm).toBeFalsy();
+  });
+
+  it('should be able to activate the new tournament form', () => {
+    const dom = fixture.nativeElement;
+
+    const tournamentButon = dom.querySelector('.button-add-tournament-start');
+    expect(tournamentButon).toBeTruthy();
+    tournamentButon.click();
+
+    const tournamentForm = dom.querySelector('.new-tournament-form');
+    expect(tournamentForm).toBeTruthy();
+  });
+
+  it('should be able to add a new tournament', () => {
+    const dom = fixture.nativeElement;
+
+    const buttonStartAdd = dom.querySelector('.button-add-tournament-start');
+    expect(buttonStartAdd).toBeTruthy();
+    buttonStartAdd.click();
+
+    const tournamentForm = dom.querySelector('.new-tournament-form');
+    expect(tournamentForm).toBeTruthy();
+
+    const inputNewName = dom.querySelector('#input-new-name');
+    inputNewName.value = tournamentName;
+
+    const inputNewTournamentDate = dom.querySelector('#input-new-tournament-date');
+    inputNewTournamentDate.value = tournamentDate;
+  });
 });
