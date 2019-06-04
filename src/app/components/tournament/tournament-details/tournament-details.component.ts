eda0318319874408bef248e2c0e597a6e7cc9ffe
Shawfe Sung <shawfe_sung@homedepot.com>
2019-06-04 07:20:27 -0400

    WIP: Adding tournament details

--- src/app/components/tournament/tournament-details/tournament-details.component.ts
+++ src/app/components/tournament/tournament-details/tournament-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tournament-details',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tournament-details.component.scss']
 })
 export class TournamentDetailsComponent implements OnInit {
+  tournamentUrl: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.tournamentUrl = this.route.snapshot.queryParamMap.get("tournament");
   }
 
 }
