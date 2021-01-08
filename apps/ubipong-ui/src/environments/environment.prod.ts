27f13260ae2ca039e5fa8485c81355237afebc58
Shawfe Sung <shawfe@techie.com>
2021-01-08 09:24:58 -0500

    Fixed refresh interval on environment.prod.ts

--- apps/ubipong-ui/src/environments/environment.prod.ts
+++ apps/ubipong-ui/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   mockData: false,
   roundRobinGridRefresh: true,
-  roundRobinGridRefreshTime: 15000,
+  roundRobinGridRefreshInterval: 15000,
   tournamentServiceUrl: "https://ubipong-api.herokuapp.com",
 };
