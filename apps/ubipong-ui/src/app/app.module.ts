aacd7dbc9ab1948b715c4eac20591b14b6c52010
Shawfe Sung <shawfe@techie.com>
2021-02-08 08:00:14 -0500

    Added a bunch of Material modules

--- apps/ubipong-ui/src/app/app.module.ts
+++ apps/ubipong-ui/src/app/app.module.ts
@@ -11,8 +11,24 @@ import { FormsModule } from '@angular/forms'
 import { TournamentPageComponent } from './pages/tournament-page/tournament-page.component'
 import { TournamentListComponent } from './components/tournament-list/tournament-list.component'
 import { DashboardComponent } from './pages/dashboard/dashboard.component'
-import { RoundRobinPageComponent } from './pages/round-robin-page/round-robin-page.component';
+import { RoundRobinPageComponent } from './pages/round-robin-page/round-robin-page.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatChipsModule } from '@angular/material/chips'
+import { MatDatepickerModule} from '@angular/material/datepicker'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatSelectModule } from '@angular/material/select'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatTableModule } from '@angular/material/table'
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 @NgModule({
   declarations: [
@@ -30,6 +46,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatToolbarModule,
   ],
   providers: [
     TournamentService,
