ce028cce59842462d1a926d27b5170e01eddcde9
Shawfe Sung <shawfe@techie.com>
2021-02-09 20:57:24 -0500

    Redoing dashboard

--- apps/ubipong-ui/src/app/components/tournament-list/tournament-list.component.spec.ts
+++ apps/ubipong-ui/src/app/components/tournament-list/tournament-list.component.spec.ts
@@ -7,6 +7,26 @@ import { Router } from '@angular/router'
 import { RouterTestingModule } from '@angular/router/testing'
 import { TournamentService } from '../../services/tournament.service'
 import { Tournament } from '../../models/tournament'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatChipsModule } from '@angular/material/chips'
+import { MatDatepickerModule} from '@angular/material/datepicker'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatSelectModule } from '@angular/material/select'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatTableModule } from '@angular/material/table'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { LayoutModule } from '@angular/cdk/layout'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 
 describe('TournamentListComponent', () => {
   const tournamentLink = "http://localhost:8080/crud/tournaments/1";
@@ -90,6 +110,26 @@ describe('TournamentListComponent', () => {
       imports: [ 
         FormsModule,
         RouterTestingModule.withRoutes([]),
+        NoopAnimationsModule,
+        MatMenuModule,
+        MatButtonModule,
+        MatCardModule,
+        MatChipsModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatFormFieldModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatProgressSpinnerModule,
+        MatSelectModule,
+        MatSnackBarModule,
+        MatTableModule,
+        MatToolbarModule,
+        LayoutModule,
+        MatSidenavModule,
+        MatListModule,
       ],
     })
     .compileComponents();
